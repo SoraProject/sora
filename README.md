09583cac191e37f49a9f6a3ef3192d622eada6df
Saroniii <54314408+Saroniii@users.noreply.github.com>
2020-12-12 09:45:58 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## dpy_cog_template
+
+A template for developing in discord.py using the Cog (commands framework).
+
+## How to use
+
+・Put the bot client's token in the environment variable named `TOKEN` and start it.
+
+## How to check the operation
+
+・If you type `!goodmorning` and there is a response, it is working properly.
+
+## How to make sure the Bot works
+
+・You can change this by rewriting the `command_prefix` variable on line 8 in `main.py`.
---
+++ main.py
@@ -0,0 +1,37 @@
+# coding: UTF-8
+import discord
+from discord.ext import commands
+import os
+import pathlib
+from utils import cog_loader as loader
+
+TOKEN = os.environ['TOKEN']
+command_prefix = ['!'] #Prefix
+
+class MyBot(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        self.ready_check = False  # Variable to prevent duplicate on_ready events from being triggered
+        super().__init__(*args, **kwargs)
+
+    async def on_ready(self):
+
+        if self.ready_check == False:
+
+            print('Logged in as')
+            print(self.user.name)
+            print(self.user.id)
+            print(f'import')
+            folder_name = 'cogs'
+            loader().cog_load(self, f'{folder_name}/*.py')
+
+            print('------')
+            self.ready_check = True
+        
+        else:
+            print('The start up process is already complete!')
+
+
+intent: discord.Intents = discord.Intents.default()
+bot = MyBot(command_prefix=command_prefix, intents=intent)
+
+bot.run(TOKEN)
