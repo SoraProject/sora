2da5ea5a20367502d758e5afb27d2165c6c320ad
Saroniii <54314408+Saroniii@users.noreply.github.com>
2020-12-12 23:12:15 +0900

    fix: 再帰処理のエラーを修正

--- utils/dropbox.py
+++ utils/dropbox.py
@@ -7,7 +7,7 @@ class Dropbox:
     def __init__(self):
         self.database = Database()
         self.DROPBOX_TOKEN = os.environ["DROPBOX_TOKEN"]
-        self.dbx = Dropbox()
+        self.dbx = dropbox
         self.database_name = "sora.db"
 
     def upload_database(self, database_name =None):
