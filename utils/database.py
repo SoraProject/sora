d9c7989a2cca190b33d543e429da54ff9fac26e1
Saroniii <sarotube0211@gmail.com>
2020-12-12 22:21:41 +0900

    feat: データベースから効率よくデータを取得/追加できる関数の追加

--- utils/database.py
+++ utils/database.py
@@ -19,4 +19,53 @@ class Database:
         
         return False
 
+    async def fetch_database(self, table_name, db =None):
+        """
+        データベースから指定したテーブルのデータをすべて取得します。
+        """
+        if not self.has_database():
+            return None
+
+        if not db:
+            db = self.database_name
+
+        conn: aiosqlite3.Connection = await aiosqlite3.connect(db)
+        c: aiosqlite3.Cursor = await conn.cursor()
+        
+        datas = []
+        
+        try:
+            async for row in c.execute(f"SELECT * FROM {table_name}"):
+                datas += row
+        
+        except:
+            return None
+        
+        return datas
+
+    async def add_data(self, table_name, data: list, db =None):
+        """
+        指定したテーブルにデータを1つ追加します。
+        """
+        if not self.has_database():
+            return None
+        
+        if not db:
+            db = self.database_name
+
+        conn: aiosqlite3.Connection = await aiosqlite3.connect(db)
+        c: aiosqlite3.Cursor = await conn.cursor()
+
+        await c.execute(f"INSERT INTO {table_name} VALUES({[i for i in data]})")
+        await conn.commit()
+        await conn.close()
+
+    async def add_datas(self, table_name, datas: list, db =None):
+        """
+        指定したテーブルにデータを複数個追加します。
+        """
+        for data in datas:
+            await self.add_data(table_name, data, db=None)
+
+        return True
     
