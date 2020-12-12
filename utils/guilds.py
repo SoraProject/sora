97c9e2390dd18703daf268abb09236a7163c84d9
Saroniii <sarotube0211@gmail.com>
2020-12-12 22:20:26 +0900

    feat: 指定したオブジェクトとギルドが一致するか確認するクラスの追加

--- utils/guilds.py
+++ utils/guilds.py
@@ -1,10 +1,25 @@
+import discord
+
+
 class WafGuild:
 
     def __init__(self):
         self.guild_id = 706513089541046303
+        self.whitelist_table = "waf_white_list"
 
 class SkyGuild:
 
     def __init__(self):
         self.guild_id = 705629561743736935
+
+class CheckGuild:
+
+    def is_guild(self, guild: discord.Guild, guild_obj: object):
+        """
+        指定したオブジェクトが指定したギルドのものか確認します。
+        """
+        if guild.id == guild_obj().guild_id:
+            return True
+        
+        return False
 
