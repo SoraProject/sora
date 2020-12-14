a9b16283494127294dd11379e58f1a0bf9ce3832
Saroniii <sarotube0211@gmail.com>
2020-12-14 11:03:39 +0900

    fix: ID指定ミスを修正

--- utils/permissions.py
+++ utils/permissions.py
@@ -9,7 +9,7 @@ class BotPermission:
         指定したユーザーがBot管理者か確認します。
         """
         try:
-            if [i for i in (await (await bot.fetch_guild(787276328985952276)).fetch_member(ctx.author.id)).roles if i.id == 787276328985952276]:
+            if [i for i in (await (await bot.fetch_guild(706513089541046303)).fetch_member(ctx.author.id)).roles if i.id == 787276328985952276]:
                 return True
             else:
                 return False
