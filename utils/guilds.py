efcc97516852bb69e7c98153ff979118226faa68
Saroniii <sarotube0211@gmail.com>
2021-02-12 12:15:47 +0900

    feat: ゆるもば！を追加

--- utils/guilds.py
+++ utils/guilds.py
@@ -7,19 +7,26 @@ class WafGuild:
         self.guild_id = 706513089541046303
         self.whitelist_table = "waf_white_list"
 
+
 class SkyGuild:
 
     def __init__(self):
         self.guild_id = 705629561743736935
 
+
+class MLBBGuild:
+
+    def __init__(self):
+        self.guild_id = 807562875996209172
+
 class CheckGuild:
 
-    def is_guild(self, guild: discord.Guild, guild_obj: object):
+    @staticmethod
+    def is_guild(guild: discord.Guild, guild_obj):
         """
         指定したオブジェクトが指定したギルドのものか確認します。
         """
         if guild.id == guild_obj().guild_id:
             return True
-        
-        return False
 
+        return False
